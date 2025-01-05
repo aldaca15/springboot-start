6f80abfe0b8d83a0ccf12208cccf6590af824c52
Ali Adame <aliadame15@gmail.com>
2025-01-04 18:02:15 -0800

    test: more imperative vs declarative examples with streams

--- src/test/java/com/modernjava/ImperativeVsDeclarativeTest.java
+++ src/test/java/com/modernjava/ImperativeVsDeclarativeTest.java
@@ -2,6 +2,10 @@ package com.modernjava;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,6 +37,31 @@ public class ImperativeVsDeclarativeTest {
     }
 
     @Test
+    public void sumOnlyPairsWithImperativeProgramming() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // filter and sum pairs, imperative
+        int total = 0;
+        for(Integer i : numbers) {
+            if(i % 2 == 0) {
+                total += i;
+            }
+        }
+        System.out.println("Total should be 30 and is " + total);
+        assertEquals(30, total); // Sum of 2, 4, 6, 8, and 10
+    }
+
+    @Test
+    public void sumOnlyPairsWithDeclarativeProgramming() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int total = numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(Integer:: intValue)
+                .sum();
+        System.out.println("Total should be 30 and is " + total);
+        assertEquals(30, total); // Sum of 2, 4, 6, 8, and 10
+    }
+
+    @Test
     public void stringWithImperativeProgramming() {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= 5; i++) {
@@ -47,5 +76,26 @@ public class ImperativeVsDeclarativeTest {
         StringBuilder sb = new StringBuilder();
         numbers.forEach(sb::append);
         assertEquals(oneToFiveStr, sb.toString());
+    }
+
+    @Test
+    public void nameListToUpperWithImperativeProgramming() {
+        List<String> names = Arrays.asList("Ana", "Peter", "Louis", "Maria");
+        List<String> upperCaseNames = new ArrayList<>();
+        for (String name : names) {
+            upperCaseNames.add(name.toUpperCase());
+        }
+        assertEquals(names.get(0).toUpperCase(), upperCaseNames.get(0)); // ANA
+        assertEquals(names.get(names.size()-1).toUpperCase(), upperCaseNames.get(names.size()-1)); // MARIA
+    }
+
+    @Test
+    public void nameListToUpperWithDeclarativeProgramming() {
+        List<String> names = Arrays.asList("Ana", "Peter", "Louis", "Maria");
+        List<String> upperCaseNames = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        assertEquals(names.get(0).toUpperCase(), upperCaseNames.get(0)); // ANA
+        assertEquals(names.get(names.size()-1).toUpperCase(), upperCaseNames.get(names.size()-1)); // MARIA
     }
 }
